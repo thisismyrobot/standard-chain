96722d6da97db05aa20dbf2c65c1d6486f8fcffc
Robert Wallhead <rwallhead@gmail.com>
2018-06-23 11:41:01 +1000

    Basic restore code

--- StandardChain/BlockChain.cs
+++ StandardChain/BlockChain.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
@@ -12,10 +13,25 @@ namespace StandardChain
         private readonly IList<Block<T>> _chain;
         private readonly HashAlgorithm _hasher;
 
-        public BlockChain()
+        public BlockChain(HashAlgorithm hasher)
         {
+            _hasher = hasher ?? throw new ArgumentNullException(nameof(hasher));
             _chain = new List<Block<T>>();
-            _hasher = SHA512.Create();
+        }
+
+        public BlockChain(string serialisedChain)
+        {
+            var chain = JsonConvert.DeserializeObject<IList<Block<T>>>(serialisedChain);
+            string lastHash = null;
+            foreach(var block in chain)
+            {
+                var blockHash = block.Hash(_hasher);
+                if (lastHash != null && blockHash != lastHash) throw new InvalidBlockChainException();
+
+                chain.Add(block);
+
+                lastHash = blockHash;
+            }
         }
 
         public void AddBlock(T transaction, DateTime timeStamp)
@@ -23,6 +39,11 @@ namespace StandardChain
             var hash = _lastBlock?.Hash(_hasher) ?? "";
             var block = new Block<T>(transaction, timeStamp, hash);
             _chain.Add(block);
+        }
+
+        public string Serialise()
+        {
+            return JsonConvert.SerializeObject(_chain);
         }
     }
 }
---
+++ StandardChain/InvalidBlockChainException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StandardChain
+{
+    [Serializable]
+    internal class InvalidBlockChainException : Exception
+    {
+        public InvalidBlockChainException()
+        {
+        }
+
+        public InvalidBlockChainException(string message) : base(message)
+        {
+        }
+
+        public InvalidBlockChainException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
