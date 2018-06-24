a22ba8f26742054f5808c2d6d8bdbb683b4ef827
Robert Wallhead <rwallhead@gmail.com>
2018-06-24 18:50:30 +1000

    Works!

---
+++ StandardChain.Tests/GlobalSuppressionsTestsOnly.cs
@@ -0,0 +1,5 @@
+﻿[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "CA5351:Do not use insecure cryptographic algorithm MD5.")]
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
